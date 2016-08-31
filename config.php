d9a4796d50fbcecb736ad0e307d6669de8e6e795
Josha von Gizycki <mail@joshavg.de>
2016-08-31 16:50:12 +0200

    removed local path

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 // configure - without trailing slash
-define('IMAGE_DIR', '/home/jgizycki/public_html/instantgallery/images');
+define('IMAGE_DIR', '/some/path');
 define('THUMB_SIZE_MAX_WIDTH', 500);
 define('THUMB_SIZE_MAX_HEIGHT', 300);
 
